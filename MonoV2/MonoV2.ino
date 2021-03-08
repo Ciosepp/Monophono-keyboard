47eb8d89e90a50ab958ce8536119d225b446a546
Ugo Baroncini <ugobaroncini@gmail.com>
2021-03-08 14:57:27 +0100

    Guidelines for a better structure

--- MonoV2/MonoV2.ino
+++ MonoV2/MonoV2.ino
@@ -1,31 +1,47 @@
-//MonoV2.ino
-//main file
+// MonoV2.ino
+// main file
 #include "pin-configuration-board-v2.h"
 #include "keyboard.h"
 #include "arpeggiator.h"
-
-
 
 void setup(){
     pinModeInit(); 
     Serial.begin(115200);
     Serial.print("Mono keyboard firmware 2.0 rev.C");
 
-    for (int i = 0; i > N; i++) {
-        oldkeyboard[i] = 0;
-    }
+    // for (int i = 0; i < N; i++) {
+    //     oldkeyboard[i] = 0;
+    // }
 
-    dacInit();
+    // dacInit();
 }
 
 int oldnote = 0;
 
 void loop(){
-    n = mono(oldnote);
-    oldnote = n;
-    if (debugEnable) {
-        SerialDebug();
+    // lettura controlli tastiera
+
+    // lettura tastiera
+
+    if (mode == MONO) {
+        monoMain(statoControlli, statoTastiera);
     }
+
+    if (mode == ARPEGGIATOR) {
+        arpeggiatorOldState = arpeggiatorMain(statoControlli,
+                                              statoTastiera,
+                                              arpeggiatorOldState);
+    }
+
+    // output controlli (led, schermi, etc)
+
+
+    // roba di Giuseppe
+    // n = mono(oldnote);
+    // oldnote = n;
+    // if (debugEnable) {
+    //     SerialDebug();
+    // }
 }
 
 
